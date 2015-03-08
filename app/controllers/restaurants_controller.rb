d873221faeff1c38450716a24db6f335d638adcb
ChristopherElten <Chris.Elten@gmail.com>
2015-03-08 13:44:17 -0400

    CRUD for restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,25 @@
 class RestaurantsController < ApplicationController
 
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def new
     @restaurant = Restaurant.new
   end
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -14,15 +28,14 @@ class RestaurantsController < ApplicationController
 
   def update
     @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
   end
 
-  def index
-    @restaurants = Restaurant.all
-  end
-
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
 
 #todo  
   def destroy
